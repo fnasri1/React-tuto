b124e1f86644e506378de9889fc63b89f4ef19df
fnasri1 <101104165+fnasri1@users.noreply.github.com>
2024-01-22 16:52:28 +0100

    github app test commit

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,7 +5,8 @@ function ListGroup(){
             <li className="list-group-item">A second item</li>
             <li className="list-group-item">A third item</li>
             <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
+            <li className="list-group-item">A fifth one</li>
+            <li className="list-group-item">And a sixth one</li>
         </ul> 
    )
 }
